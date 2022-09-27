c066ccf36f8a291f0753c697798a65986e7229d8
lizz-kuzz <111276778+lizz-kuzz@users.noreply.github.com>
2022-09-27 13:29:44 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,13 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-
-
 FILE *logs_;
-//  понять что делает # ## этапы компиляции предпроцессор 
-
-// 1 этап условной компиляции вызвать разные структуры для разных модов
-// сделать конструктор расширенный
 
 int main() {
     const char *LOG_FILE = "/mnt/c/Users/User/Desktop/programs/stack/log.txt";
@@ -28,6 +22,7 @@ int main() {
     stack stk1 = {};
 
     stack_ctor(stk1, 10);
+    
     for (int i = 1; i <= 40; i++) {
         stack_push(&stk1, i);
     }
@@ -37,6 +32,7 @@ int main() {
     for (int i = 40; i > 0; i--) {
         stack_pop(&stk1, &value);
     }
+
     stack_dtor(&stk1);
 
     fclose(logs_);
